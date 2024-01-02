e23d38847b04f1f8940d1c3ff8f73a7b9679df29
Zel Start <134749644+zelstart@users.noreply.github.com>
2024-01-02 14:52:27 -0500

    put secret in dotenv file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,9 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = process.env.JWT_SECRET || 'secret';
 const expiration = '2h';
 
 module.exports = {
